457933f60fed4298766a116d8537e45f08878ec3
Darcy Boddey <boddeydarcy@gmail.com>
2024-03-12 20:12:55 +0000

    amend Loan.java

--- Loan.java
+++ Loan.java
@@ -1,3 +1,26 @@
 abstract class Loan{
+    private String RecordID;
+    private String loanType;
+    private int interestRate;
+    private int loanTermLeft;
+}
+
+class Auto extends Loan{
+
+}
+
+class Builder extends Loan{
+
+}
+
+class Mortgage extends Loan{
+
+}
+
+class Personal extends Loan{
+
+}
+
+class otherLoan extends Loan{
 
 }
